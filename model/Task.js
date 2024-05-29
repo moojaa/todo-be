90243b35f67cdb5c62c3f0e6c5a9331ba9db5b24
moojaa <129938297+moojaa@users.noreply.github.com>
2024-05-29 23:00:02 +0900

    로그인심화

--- model/Task.js
+++ model/Task.js
@@ -2,16 +2,21 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const taskSchema = Schema({
-    task:{
-        type:String,
-        required:true
+    task: {
+        type: String,
+        required: true
     },
-    isComplete:{
-        type:Boolean,
-        required:true
+    isComplete: {
+        type: Boolean,
+        required: true
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: "User"
     }
-},{timestamps:true})
+}, { timestamps: true })
 
-const Task = mongoose.model("Task",taskSchema)
+const Task = mongoose.model("Task", taskSchema)
 
-module.exports= Task
+module.exports = Task
